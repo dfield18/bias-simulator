1f30dfe93fe48d960bb72eba4b5a9318ce5f7fe4
dfield18 <davidcharlesfield@gmail.com>
2026-04-15 10:06:42 -0400

    Load GTM unconditionally on every page, keep GA/Ads consent-gated

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -57,7 +57,19 @@ export default function RootLayout({
       },
     }}>
       <html lang="en">
+        <head>
+          {/* GTM loads unconditionally on every page */}
+          <script dangerouslySetInnerHTML={{ __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5WJ9564T');
+          `}} />
+        </head>
         <body className="bg-gray-950 text-gray-100 min-h-screen">
+          {/* GTM noscript fallback */}
+          <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5WJ9564T" height="0" width="0" style={{ display: "none", visibility: "hidden" }} /></noscript>
           <AuthProvider>{children}</AuthProvider>
           <CookieBanner />
           <GoogleAnalytics />
